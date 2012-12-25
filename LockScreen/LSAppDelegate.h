e2259dff72f2e6d1b0eb8fa565315764abbb4bc7
Brian Turner <turner@bignerdranch.com>
2012-12-25 16:51:28 -0500

    Initial Commit

---
+++ LockScreen/LSAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  LSAppDelegate.h
+//  LockScreen
+//
+//  Created by Brian Turner on 12/25/12.
+//  Copyright (c) 2012 Turningdevelopment. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LSAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ LockScreen/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  LockScreen
+//
+//  Created by Brian Turner on 12/25/12.
+//  Copyright (c) 2012 Turningdevelopment. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "LSAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([LSAppDelegate class]));
+    }
+}
