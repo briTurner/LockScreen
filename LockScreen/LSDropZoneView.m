7b96082fceef4b22c418e5d6c6e27cfad68583f6
Brian Turner <turner@bignerdranch.com>
2013-05-02 14:21:30 -0400

    spacing of characters in drop zone is a little better

--- LockScreen/LSDropZoneView.m
+++ LockScreen/LSDropZoneView.m
@@ -9,6 +9,9 @@
 #import "LSDropZoneView.h"
 #import "LSPasswordCharacter.h"
 
+const int kMaxCharsPerRow = 6;
+
+
 @interface LSDropZoneView () {
     NSMutableArray *_characters;
     NSMutableArray *_characterViews;
@@ -40,11 +43,20 @@
 
 - (void)layoutSubviews {
     if ([_characterViews count]) {
-        int x = 0;
-        int charWidth = MIN(320 / [_characterViews count], 100);
-        for (UIImageView *characterImageView in _characterViews) {
-            [characterImageView setFrame:CGRectMake(charWidth * x, 0, charWidth, charWidth)];
-            x++;
+
+        int numberOfRows = ceil([_characterViews count] / (float)kMaxCharsPerRow);
+
+        int charWidth = MAX(MIN(320 / [_characterViews count], 100), 53);
+
+        int index = 0;
+        for (int y = 0; y < numberOfRows; y++) {
+            for (int x = 0; x < kMaxCharsPerRow; x++) {
+                if (index >= [_characterViews count])
+                    break;
+                UIImageView *characterImageView = [_characterViews objectAtIndex:index];
+                [characterImageView setFrame:CGRectMake(charWidth * x, charWidth * y, charWidth, charWidth)];
+                index++;
+            }
         }
     }
 }
