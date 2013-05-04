ac214427b0f9fc2e7912ea6639c54ababccafe37
Brian Turner <turner@bignerdranch.com>
2013-05-04 16:52:28 -0400

    completely changed file structure

---
+++ Lock Screen Library/LSLockScreen.h
@@ -0,0 +1,17 @@
+//
+//  Header.h
+//  LockScreenSimpleDemo
+//
+//  Created by Brian Turner on 5/4/13.
+//  Copyright (c) 2013 Turning Development. All rights reserved.
+//
+
+#ifndef LockScreenSimpleDemo_Header_h
+#define LockScreenSimpleDemo_Header_h
+
+#import "LSLockScreenViewController.h"
+#import "LSPasswordPickerViewController.h"
+#import "LSPassword.h"
+#import "LSPasswordCharacter.h"
+
+#endif
